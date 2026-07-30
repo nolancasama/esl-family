f1260c4fb4c3766bce4343a198d2cb1417bbd517
nolancasama <nolancasama@gmail.com>
2026-07-30 15:57:50 +0900

    Adjust selfie and completion badge spacing

--- js/main.js
+++ js/main.js
@@ -652,8 +652,8 @@
           '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" ' +
           'stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round">' +
           '<polyline points="20 6 9 17 4 12"/></svg>';
-        mark.style.left = `${rect.left + rect.width - 10}px`;
-        mark.style.top  = `${rect.top + 10}px`;
+        mark.style.left = `${rect.left + rect.width - 24}px`;
+        mark.style.top  = `${rect.top + 24}px`;
         completionMarks.appendChild(mark);
       });
   }
